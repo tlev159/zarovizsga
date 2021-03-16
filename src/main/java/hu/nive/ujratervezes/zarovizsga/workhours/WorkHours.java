2a86aea3189d7ee633d8785fa4acad50beae609b
tlev159 <t.levente1598@gmail.com>
2021-03-16 17:38:42 +0100

    day with the minimum worked hour

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -0,0 +1,33 @@
+package hu.nive.ujratervezes.zarovizsga.workhours;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WorkHours {
+
+  public String minWork(String file) {
+    try (BufferedReader reader = Files.newBufferedReader(Path.of(file))) {
+      String result = null;
+      String line;
+      result = getMinWorkedDay(reader, result);
+      return result;
+    } catch (IOException ioe) {
+      throw new IllegalStateException("Can not read file!", ioe);
+    }
+  }
+
+  private String getMinWorkedDay(BufferedReader reader, String result) throws IOException {
+    int minWorkedHours = 24;
+    String line;
+    while ((line = reader.readLine()) != null) {
+      String[] temp = line.split(",");
+      if (Integer.parseInt(temp[1]) < minWorkedHours) {
+        result = temp[0] + ": " + temp[2];
+        minWorkedHours = Integer.parseInt(temp[1]);
+      }
+    }
+    return result;
+  }
+}
